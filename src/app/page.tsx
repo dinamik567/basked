6de6f402ea3b6ceaa8cbd8733e99f01a3a7b4adf
dinamik567 <irhin2011@yandex.ru>
2024-05-20 20:57:07 +0300

    Накинул стилей на header и создал форму для создания корзины

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [quantity, setQuantity] = useState(1);
+
+  function handleChangeInputQuantity(e: React.FormEvent<HTMLInputElement>) {
+    const value = Number(e.currentTarget.value);
+    if (value >= 1) {
+      setQuantity(value);
+    }
+    return;
+  }
   return (
     <main className={styles.main}>
-      <h1>Главная</h1>
-      <form>
-        <input />
+      <h1 className={styles.title}>Главная</h1>
+      <form className={styles.form}>
+        <label className={styles.label}>
+          Сколько товаров добавить в корзину
+        </label>
+        <input
+          className={styles.input}
+          onChange={(e) => handleChangeInputQuantity(e)}
+          value={quantity}
+          type="number"
+        />
+        <input
+          className={styles.submitButton}
+          value="Создать корзину"
+          type="submit"
+        />
       </form>
     </main>
   );
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,42 +1,36 @@
 import Link from "next/link";
+import styles from "./styles.module.css";
 
 export function Header() {
   return (
-    <header className="header">
-      <div className="logo">
-        <a href="/">Logo</a>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">Logo</Link>
       </div>
       <nav>
-        <ul
-          style={{
-            display: "flex",
-            listStyleType: "none",
-            justifyContent: "space-between",
-            fontWeight: "bold",
-          }}
-        >
-          <li>
+        <ul className={styles.navList}>
+          <li className={`${styles.navItem} ${styles.dropdown}`}>
             <Link href="#">Каталог</Link>
-            {/* <select>
-              <option>Корма для животных</option>
-              <option>Средства ухода</option>
-              <option>Игрушки для животных</option>
-              <option>Аксессуары</option>
-            </select> */}
+            <div className={styles.dropdownContent}>
+              <Link href="/catalog/food">Корма для животных</Link>
+              <Link href="/catalog/care">Средства ухода</Link>
+              <Link href="/catalog/toys">Игрушки для животных</Link>
+              <Link href="/catalog/accessories">Аксессуары</Link>
+            </div>
           </li>
-          <li>
+          <li className={styles.navItem}>
             <Link href="/orders">Заказы</Link>
           </li>
-          <li>
+          <li className={styles.navItem}>
             <Link href="/favorites">Избраное</Link>
           </li>
-          <li>
+          <li className={styles.navItem}>
             <Link href="/stocks">Акции</Link>
           </li>
-          <li>
+          <li className={styles.navItem}>
             <Link href="/contacts">Контакты</Link>
           </li>
-          <li>
+          <li className={styles.navItem}>
             <Link href="/basket">Корзина</Link>
           </li>
         </ul>
